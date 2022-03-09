7bb8c081581417c10ef674215ed7225bdf92fc2c
Karol <kariurek@gmail.com>
2022-03-09 11:21:06 +0100

    api spam prevent

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 let catCount = 0;
+let canCountPlacement = 0;
 function scrollHandler() {
 	const distanceToBottom = document.body.getBoundingClientRect().bottom;
-	// console.log(document.documentElement.clientHeight, distanceToBottom);
-	if (distanceToBottom < document.documentElement.clientHeight + 150) {
+	if (distanceToBottom < document.documentElement.clientHeight + 1200) {
 		fetch('https://api.thecatapi.com/v1/images/search')
 			.then(response => response.json())
 			.then(data => {
-				const newDataElement = document.createElement('div');
-				newDataElement.classList = 'cat-container';
-				console.log(data);
-				catCount = catCount + 1;
-				newDataElement.innerHTML = `<img src="${data[0].url}">
-                <p>Cat number ${catCount}</p>`;
-				document.body.append(newDataElement);
+				if (canCountPlacement < 3) {
+					const newDataElement = document.createElement('div');
+					newDataElement.classList = 'cat-container';
+					// console.log(data);
+					catCount = catCount + 1;
+					canCountPlacement = canCountPlacement + 1;
+					newDataElement.innerHTML = `<img src="${data[0].url}">
+                                    <p>Cat number ${catCount}</p>`;
+					document.body.append(newDataElement);
+				} else if (
+					distanceToBottom <
+					document.documentElement.clientHeight + 1200
+				) {
+					canCountPlacement = 0;
+					console.log('reset', distanceToBottom);
+				}
 			});
 	}
 }
